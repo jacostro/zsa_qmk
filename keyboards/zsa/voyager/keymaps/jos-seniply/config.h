105cd105d2b7f8246aecc4332a4e232a1a151904
Jacek Ostrowski <jacostro@gmail.com>
2025-05-01 11:38:53 +0200

    jos-seniply-8

---
+++ keyboards/zsa/voyager/keymaps/jos-seniply/config.h
@@ -0,0 +1,13 @@
+#define USB_SUSPEND_WAKEUP_DELAY 0
+#define AUTO_SHIFT_MODIFIERS
+#define NO_AUTO_SHIFT_TAB
+#define NO_AUTO_SHIFT_SPECIAL
+#define NO_AUTO_SHIFT_NUMERIC
+#define HOLD_ON_OTHER_KEY_PRESS
+#define SERIAL_NUMBER "OQPyr/9DQXR3"
+#define LAYER_STATE_8BIT
+#define HCS(report) host_consumer_send(record->event.pressed ? report : 0); return false
+
+#define RGB_MATRIX_STARTUP_SPD 60
+
+#define CAPS_LOCK_STATUS
