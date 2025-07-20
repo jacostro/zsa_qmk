764e4cf7d22062efbfcbbcc23a62c1e476e09e12
Jacek Ostrowski <jacostro@gmail.com>
2025-07-20 09:50:17 +0200

    jos-seniply-15.1

--- keyboards/zsa/voyager/keymaps/jos-seniply/keymap.c
+++ keyboards/zsa/voyager/keymaps/jos-seniply/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_BSPC,        LGUI(KC_1),     LGUI(KC_2),     LGUI(KC_3),     LGUI(KC_4),     LGUI(KC_5),                                     TG(3),          KC_TRANSPARENT, LGUI(KC_6),     LGUI(KC_7),     KC_TRANSPARENT, KC_DELETE,      
     KC_ESCAPE,      KC_Q,           KC_W,           KC_F,           KC_P,           KC_B,                                           KC_J,           KC_L,           KC_U,           KC_Y,           KC_SCLN,        KC_BSLS,        
     CW_TOGG,        KC_A,           KC_R,           KC_S,           KC_T,           KC_G,                                           KC_M,           KC_N,           KC_E,           KC_I,           KC_O,           KC_QUOTE,       
-    OSM(MOD_RALT),  KC_Z,           KC_X,           KC_C,           KC_D,           KC_V,                                           KC_K,           KC_H,           KC_COMMA,       KC_DOT,         KC_SLASH,       OSM(MOD_RALT),  
+    KC_RIGHT_ALT,   KC_Z,           KC_X,           KC_C,           KC_D,           KC_V,                                           KC_K,           KC_H,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_RIGHT_ALT,   
                                                     MO(1),          LT(3, KC_TAB),                                  KC_ENTER,       LT(2, KC_SPACE)
   ),
   [_EXT_LAYER] = LAYOUT_voyager(
